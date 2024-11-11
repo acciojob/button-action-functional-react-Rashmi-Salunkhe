9946575d9803d7962f6710a409e32c05ca3802f5
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-11-11 11:56:41 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,15 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [showPara,setShowPara] = useState(false);
+  function handleClick(){
+    setShowPara(!showPara)
+  }
   return (
     <div id="main">
       // Do not alter the main div
+    {showPara && <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy </p>}
+    <button id="click" onClick={handleClick}>{(showPara ? "Hide" : "Show")}</button>
     </div>
   );
 }
